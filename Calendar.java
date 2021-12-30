0891630b8c6f713c1d7443d168c2e7158d61e2ba
LEGEND <96801551+Onlwu@users.noreply.github.com>
2021-12-30 18:06:40 +0900

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -8,40 +8,11 @@ public class Calendar {
 		
 		//원하는 달력 설정
 		////////////////////////////////////////////////////////////////
-		//20.1.1 수요일 / 21.1.1 금 /22.1.1 토 /23.1.1 일 /24년 1.1 월
-		//int year = 2024;// 출력하고 싶은 년(욕아님ㅋ)
-		//int day= 1;//출력할 시작 월의 시작요일, 1. 월 2.화....7.일 1.1.1 은 월요일
 		int thisMonth =1;//출력 시작 범위
 		int MonthRange=12;//출력할 월 범위
 		////////////////////////////////////////////////////////////////
-		//int scanMonth=1;//나중에 출력
 		
 		int[] Month = {31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,31};//작년 12월부터 내년 1월까지
-		//시작 day 알리기
-		//System.out.println((year*365)%7);
-		//System.out.println(((year-2020)*365)%7);
-		/*
-		//윤년확인		
-		for(int i=4;i<=year;i+=4)
-		{
-		    if(i%400==0 || (i%4==0 && i%100!=0)) 
-		    {	    	
-		    	//System.out.println(i+"년은 윤년입니다!");
-		    	day++;//윤년확인 후 날짜 추가
-		    }
-		}
-		if(year%400==0 || (year%4==0 && year%100!=0)) 
-		{
-			Month[2]=29;
-		}
-		for(int i =1; i<scanMonth;i++)
-		{
-			day += Month[i];
-		}
-		//day+= year*365;
-		day = year *365;
-		day %= 7;
-		*/
 		
 
 
@@ -50,9 +21,7 @@ public class Calendar {
 	    int toYear=2021;//끝 년도
 	    int toMonth = 12;
 	    
-		//int day =1;
 	    //달력 출력
-		//int day =1;
 		for(;fromYear<=toYear;fromYear++)//년도
 		{
 			int day =1;
@@ -73,13 +42,8 @@ public class Calendar {
 				//System.out.println(i);
 				day += Month[i];//월 추가
 			}
-			//System.out.println("day = "+day);
-			day %= 7;
-			//System.out.println("day = "+day);
-			//day+= year*365;
 			
-			
-			
+			day %= 7;	
 			
 			if(fromYear<toYear)
 			{
@@ -96,8 +60,7 @@ public class Calendar {
 				System.out.println("오류입니다");				
 				break;
 			}
-			//System.out.println(fromMonth);
-
+			
 			for(;thisMonth<=MonthRange;thisMonth++)//월
 			{
 				System.out.printf("\n%d년 %d월 달력입니다.\n",fromYear, thisMonth);
